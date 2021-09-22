97ab1ff13cdcfd57eda265664ef345d04178f721
jahongir95 <jakhongirsagdullaev95@gmail.com>
2021-09-22 16:55:38 +0500

    right side of the project is developing

--- src/components/Add.js
+++ src/components/Add.js
@@ -10,10 +10,12 @@ import {
     Modal,
     Radio,
     RadioGroup,
+    Snackbar,
     TextField,
     Tooltip,
 } from "@material-ui/core"
 import { Add as AddIcon } from "@material-ui/icons"
+import MuiAlert from "@material-ui/lab/Alert"
 
 const useStyles = makeStyles((theme) => ({
     fab: {
@@ -36,15 +38,38 @@ const useStyles = makeStyles((theme) => ({
             height: "100vh",
         },
     },
+    form: {
+        padding: theme.spacing(2)
+    },
+    item: {
+        marginBottom: theme.spacing(3)
+    }
 }))
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 function Add() {
     const [open, setOpen] = useState(false)
+    const [openAlert, setOpenAlert] = useState(false)
     const [age, setAge] = useState('')
     const classes = useStyles()
 
     const handleChange = (e) => {
         setAge(e.target.value)
+    }
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpenAlert(false);
+    };
+
+    const handleCreate = () => {
+        setOpen(false)
+        setOpenAlert(true)
     }
     
 
@@ -71,7 +96,6 @@ function Add() {
                                 id="standard-multiline-static"
                                 multiline
                                 rows={4}
-                                variant="standard"
                                 label="Description"
                                 size="small"
                                 style={{ width: "100%" }}
@@ -79,7 +103,7 @@ function Add() {
                             />
                         </div>
                         <div className={classes.item}>
-                            <TextField select label="Status" style={{minWidth: 80}} onChange={handleChange} value={age}>
+                            <TextField select label="Status" style={{ minWidth: 80 }} onChange={handleChange} value={age}>
                                 <MenuItem value="public">Public</MenuItem>
                                 <MenuItem value="private">Private</MenuItem>
                                 <MenuItem value="unlisted">Unlisted</MenuItem>
@@ -100,12 +124,17 @@ function Add() {
                             </RadioGroup>
                         </div>
                         <div className={classes.item}>
-                            <Button variant="outlined" color="primary" style={{marginRight: 20}}>Create</Button>
-                            <Button variant="outlined" color="secondary" onClick={ () => setOpen(false)}>Cancel</Button>
+                            <Button variant="outlined" color="primary" style={{ marginRight: 20 }} onClick={handleCreate}>Create</Button>
+                            <Button variant="outlined" color="secondary" onClick={() => setOpen(false)}>Cancel</Button>
                         </div>
                     </form>
                 </Container>
             </Modal>
+            <Snackbar open={openAlert} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
+                <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                    This is a success message!
+                </Alert>
+            </Snackbar>
         </>
     )
 }
--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Container, makeStyles } from '@material-ui/core'
+import { Avatar, Container, ImageList, ImageListItem, makeStyles, Typography } from '@material-ui/core'
+import { AvatarGroup } from '@material-ui/lab'
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        paddingTop: theme.spacing(10)
-    }
+  container: {
+    paddingTop: theme.spacing(10)
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 500,
+    color: '#555',
+  }
 }))
 
 
@@ -14,7 +20,47 @@ function Rightbar() {
 
   return (
     <Container className={classes.container}>
-        <h1>hello world!</h1>
+      <Typography className={classes.title} gutterBottom>Online Friends</Typography>
+      <AvatarGroup max={6}>
+        <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+        <Avatar alt="Travis Howard" src="https://material-ui.com/static/images/avatar/2.jpg" />
+        <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/3.jpg" />
+        <Avatar alt="Agnes Walker" />
+        <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/5.jpg" />
+        <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/6.jpg" />
+        <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/7.jpg" />
+      </AvatarGroup>
+
+      <Typography className={classes.title} gutterBottom>Gallery</Typography>
+      <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=164&h=164&fit=crop&auto=format&dpr=2" alt="" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=164&h=164&fit=crop&auto=format&dpr=2" alt="" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=164&h=164&fit=crop&auto=format&dpr=2" alt="" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=164&h=164&fit=crop&auto=format&dpr=2" alt="" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=164&h=164&fit=crop&auto=format&dpr=2" alt="" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=164&h=164&fit=crop&auto=format&dpr=2" alt="" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1567306301408-9b74779a11af?w=164&h=164&fit=crop&auto=format&dpr=2" alt="" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1471357674240-e1a485acb3e1?w=164&h=164&fit=crop&auto=format&dpr=2" alt="" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="https://images.unsplash.com/photo-1589118949245-7d38baf380d6?w=164&h=164&fit=crop&auto=format&dpr=2" alt="" />
+        </ImageListItem>
+      </ImageList>
     </Container>
   )
 }
